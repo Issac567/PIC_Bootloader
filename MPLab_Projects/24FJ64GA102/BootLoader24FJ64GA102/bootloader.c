6756839f37734bc150c928a5a2cb801969068139
Atari Fan <167288874+Issac567@users.noreply.github.com>
2026-03-15 14:54:00 -0700

    Update bootloader.c

--- MPLab_Projects/24FJ64GA102/BootLoader24FJ64GA102/bootloader.c
+++ MPLab_Projects/24FJ64GA102/BootLoader24FJ64GA102/bootloader.c
@@ -185,7 +185,7 @@ void Flash_EraseApplication(void)
     UART_TxString("<StartFlashErase>");
     __delay_ms(MSG_MS_DELAY);
     
-    // PIC24 Logic: A Page is 512 words. 
+    // PIC24 Logic: A Page is 448 words. 
     // Since each word is 2 address units, we increment by 896.
     for (addr = FLASH_START; addr < FLASH_END; addr += 896)
     {
