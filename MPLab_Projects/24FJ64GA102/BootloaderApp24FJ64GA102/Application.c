6bc9ca7f8de07017c1f35d86b1031398dd87518c
Atari Fan <167288874+Issac567@users.noreply.github.com>
2026-03-24 01:34:37 -0700

    Update Application.c

--- MPLab_Projects/24FJ64GA102/BootloaderApp24FJ64GA102/Application.c
+++ MPLab_Projects/24FJ64GA102/BootloaderApp24FJ64GA102/Application.c
@@ -136,8 +136,8 @@ int main(void)
         {
             b = UART_Rx();
             
-            // If the handshake byte (0x55) is detected:
-            if (b == 0x55)              
+            // If the handshake byte (0x55 or 0xAA) is detected:
+            if (b == 0x55 || b == 0xAA)              
             {
                 
                 //Timer2_Stop(); // Enable Demo
