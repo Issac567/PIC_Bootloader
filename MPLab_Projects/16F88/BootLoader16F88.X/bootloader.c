175f9f1a6977d2905fcfe8daaa02e6a0c95ad4b3
Atari Fan <167288874+Issac567@users.noreply.github.com>
2026-04-02 11:31:54 -0700

    Update bootloader.c

--- MPLab_Projects/16F88/BootLoader16F88.X/bootloader.c
+++ MPLab_Projects/16F88/BootLoader16F88.X/bootloader.c
@@ -82,7 +82,7 @@ uint16_t Flash_ReadInstruction(uint16_t address)
     return word;                        // Full 14-bit instruction
 }
 
-// WRITE FLASH BLOCK Starting Address + 4 (8 Word or 4 Addresses in 1 call)
+// WRITE FLASH BLOCK Starting Address + 4 (8 byte or 4 Addresses in 1 call)
 void Flash_WriteBlock(uint16_t address, uint16_t *data) 
 {
     uint16_t i;
