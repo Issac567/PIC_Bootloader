f16e5a7a286085864d38fd40bb930797b014ab47
Atari Fan <167288874+Issac567@users.noreply.github.com>
2026-04-06 18:35:31 -0700

    Update bootloader.c

--- MPLab_Projects/24FJ64GA102/BootLoader24FJ64GA102/bootloader.c
+++ MPLab_Projects/24FJ64GA102/BootLoader24FJ64GA102/bootloader.c
@@ -38,7 +38,7 @@
 #define FLASH_WRITE_BLOCK    64                 // Write row size is 64 instructions (standard for GA102)
 
 #define TIMER2_COUNT        186                 // 3s 
-#define MSG_MS_DELAY        50                  // Standard pacing delay 
+#define MSG_MS_DELAY 150                      // (min 150 for BT latency) Standard pacing delay 
 
 #define LED_PIN   LATBbits.LATB4                // Use LAT for Output / Bootloader Led Status 
 #define LED_TRIS  TRISBbits.TRISB4              // Output PortB.4 pin
